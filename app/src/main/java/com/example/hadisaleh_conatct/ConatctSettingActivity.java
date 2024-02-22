d3f8cc8fa36f842d8ac4bfa56b8b05768e6ca493
hadisaleh0 <hadysaleh313@gmail.com>
2024-02-22 13:27:01 +0200

    first commit

---
+++ app/src/main/java/com/example/hadisaleh_conatct/ConatctSettingActivity.java
@@ -0,0 +1,14 @@
+package com.example.hadisaleh_conatct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConatctSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conatct_setting);
+    }
+}
